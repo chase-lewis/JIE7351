fda5b992b1ed5742c53a304865d6526e0e599dd5
Bruce Yang <bruceyang100@yahoo.com>
2018-02-04 19:19:24 -0500

    Removed logs start-all.bat starts all kafka processes make_producer.bat now takes ip of target database

--- src/make_producer.bat
+++ src/make_producer.bat
@@ -1,3 +1,3 @@
 javac -cp .;..\share\java\kafka\kafka-clients-0.11.0.1-cp1.jar;slf4j-api-1.7.25.jar;slf4j-simple-1.7.25.jar;sqljdbc42.jar DBConnectorTest.java
 javac -cp .;..\share\java\kafka\kafka-clients-0.11.0.1-cp1.jar;slf4j-api-1.7.25.jar;slf4j-simple-1.7.25.jar;sqljdbc42.jar KafkaProducerTest.java
-java -cp .;..\share\java\kafka\kafka-clients-0.11.0.1-cp1.jar;slf4j-api-1.7.25.jar;slf4j-simple-1.7.25.jar;sqljdbc42.jar KafkaProducerTest
+java -cp .;..\share\java\kafka\kafka-clients-0.11.0.1-cp1.jar;slf4j-api-1.7.25.jar;slf4j-simple-1.7.25.jar;sqljdbc42.jar KafkaProducerTest %1
