084d0790abe941f077e1feca6c512084475e16fc
Bruce Yang <bruceyang100@yahoo.com>
2018-02-07 13:04:02 -0500

    Removed unnecessary files added logs folder to gitignore

--- src/DBConnectorTest.bat
+++
@@ -1,2 +0,0 @@
-javac -cp .;..\share\java\kafka\kafka-clients-0.11.0.1-cp1.jar;slf4j-api-1.7.25.jar;slf4j-simple-1.7.25.jar;sqljdbc42.jar DatabaseQuery.java
-java -cp .;..\share\java\kafka\kafka-clients-0.11.0.1-cp1.jar;slf4j-api-1.7.25.jar;slf4j-simple-1.7.25.jar;sqljdbc42.jar DatabaseQuery
