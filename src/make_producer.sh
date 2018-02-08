be46679a6605fd5cad4fb6f7621d5382bf1ff97b
Noah <nharper32@gatech.edu>
2018-02-08 18:23:23 -0500

    push

--- src/make_producer.sh
+++ src/make_producer.sh
@@ -2,3 +2,6 @@ javac -cp "../share/java/kafka/kafka-clients-0.11.0.1-cp1.jar:slf4j-api-1.7.25.j
 java -cp ".:../share/java/kafka/kafka-clients-0.11.0.1-cp1.jar:slf4j-api-1.7.25.jar:slf4j-simple-1.7.25.jar:sqljdbc42.jar" KafkaProducerTest "$1"
 
 
+
+
+
