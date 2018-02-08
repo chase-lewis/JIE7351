8ec1ce645dcbb79dca68cb73ed040c9658358767
Noah <nharper32@gatech.edu>
2018-02-08 18:22:01 -0500

    Instantiate Branch

--- src/make_producer.sh
+++ src/make_producer.sh
@@ -1,2 +1,4 @@
 javac -cp "../share/java/kafka/kafka-clients-0.11.0.1-cp1.jar:slf4j-api-1.7.25.jar:slf4j-simple-1.7.25.jar:sqljdbc42.jar" DBConnectorTest.java KafkaProducerTest.java
 java -cp ".:../share/java/kafka/kafka-clients-0.11.0.1-cp1.jar:slf4j-api-1.7.25.jar:slf4j-simple-1.7.25.jar:sqljdbc42.jar" KafkaProducerTest "$1"
+
+
