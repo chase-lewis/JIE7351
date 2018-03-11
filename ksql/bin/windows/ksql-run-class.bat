9a1e74aba9f38e570a9fdf631f1b78d554021af7
zbolmb <zhijianli999@gmail.com>
2018-03-11 12:05:34 -0400

    added batch script for running ksql

---
+++ ksql/bin/windows/ksql-run-class.bat
@@ -0,0 +1,8 @@
+set base_dir=%~dp0..\..
+
+set CLASSPATH=""
+for %%i in (%base_dir%\share\java\ksql*.jar) do (
+	set CLASSPATH=%CLASSPATH%;"%%i"
+)
+
+java -cp "%base_dir%\share\java\ksql\*" io.confluent.ksql.Ksql local
