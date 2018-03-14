c49bcf4c5389ed6bbaea6a744fd4fa7c3b8be170
Noah <nharper32@gatech.edu>
2018-03-14 13:11:58 -0400

    property file for patient and participation, also bat files

---
+++ src/patient_producer.bat
@@ -0,0 +1,1 @@
+..\bin\windows\connect-standalone.bat ..\etc\kafka\connect-standalone.properties ..\etc\cdc_properties\PATIENT.properties
--- src/standalone_producer.bat
+++ src/standalone_producer.bat
@@ -1,1 +1,1 @@
-..\bin\windows\connect-standalone.bat ..\etc\kafka\connect-standalone.properties ..\etc\kafka-connect-jdbc\NBS.properties
+..\bin\windows\connect-standalone.bat ..\etc\kafka\connect-standalone.properties ..\etc\cdc_properties\NBS.properties
