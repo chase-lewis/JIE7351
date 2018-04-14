f92074cf952315902fd10bca90127904cc66bcb1
zbolmb <zhijianli999@gmail.com>
2018-04-14 14:45:50 -0400

    added general connector (untested) and started work on process_race

---
+++ src/Connector.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class Connector {
+	//private String ip = "128.61.18.34";
+	private String connectionUrl[] = {"jdbc:sqlserver://", ":1433;user=test;password=test;"};
+	private ResultSet results;
+	private Connection connection;
+
+	public Connector(String ip) {
+		// Load SQL Server JDBC driver and establish connection.
+		try{
+			String url = connectionUrl[0] + ip + connectionUrl[1];
+			System.out.print("Connecting to SQL Server " + ip + " ... ");
+			connection = DriverManager.getConnection(url);
+			System.out.println("Connected.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void query(String queryString) {
+		try {
+			Statement statement = connection.createStatement();
+			results = statement.executeQuery(queryString);
+				// System.out.println(results);
+		} catch (Exception e) {
+			System.out.println();
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet getResults() {
+		return results;
+	}
+}
--- src/KafkaConsumerTest.java
+++ src/KafkaConsumerTest.java
@@ -16,16 +16,14 @@ public class KafkaConsumerTest implements Runnable {
   private final AtomicBoolean shutdown;
   private final CountDownLatch shutdownLatch;
   private final HashMap<String, String> ops;
-  private final ODSConnector odsConnector;
-  private final RDBConnector rdbConnector;
+  private final Connector connector;
 
   public KafkaConsumerTest(Properties config, List<String> topics, String ip) {
     this.consumer = new KafkaConsumer<>(config);
     this.topics = topics;
     this.shutdown = new AtomicBoolean(false);
     this.shutdownLatch = new CountDownLatch(1);
-    this.odsConnector = new ODSConnector(ip);
-    this.rdbConnector = new RDBConnector(ip);
+    this.connector = new Connector(ip);
 
     ops = new HashMap<>();
     ops.put("I", "Insert ");
@@ -41,6 +39,8 @@ public class KafkaConsumerTest implements Runnable {
     	process_person(record.value());
     } else if (topic.equals("sql-jdbc-tables-Postal_locator")) {
     	process_postal(record.value());
+    } else if topic.equals("sql-jdbc-tables-Person_race")) {
+      process_race(record.value());
     } else {
     	System.out.println("Record is not from Person or Postal Locator");
     }
@@ -57,9 +57,9 @@ public class KafkaConsumerTest implements Runnable {
       String sql = String.format("update RDB.dbo.S_PATIENT "
                                + "set PATIENT_FIRST_NAME = '%s', "
                                + "PATIENT_LAST_NAME = '%s' "
-                               + "where PATIENT_UID = %d;", 
+                               + "where PATIENT_UID = %d;",
                                person_first, person_last, person_uid);
-      rdbConnector.query(sql);
+      connector.query(sql);
     } catch(JSONException e) {
 		e.printStackTrace();
 	}
@@ -76,8 +76,8 @@ public class KafkaConsumerTest implements Runnable {
       String street_addr2 = (json.isNull("street_addr2")) ? null : json.getString("street_addr2");
 	    System.out.println(String.format("%s : %s : %s : %s : %d", city, state, street_addr1, street_addr2, postal_locator_uid));
 	    String sql = "select entity_uid from NBS_ODSE.dbo.Entity_locator_participation where locator_uid = " + postal_locator_uid;
-	    odsConnector.query(sql);
-      ResultSet result = odsConnector.getResults();
+	    connector.query(sql);
+      ResultSet result = connector.getResults();
       int entity_uid = 0;
       while (result.next()) {
         entity_uid = result.getInt(1);
@@ -88,13 +88,74 @@ public class KafkaConsumerTest implements Runnable {
                         + "PATIENT_STATE = '%s', "
                         + "PATIENT_STREET_ADDRESS_1 = '%s', "
                         + "PATIENT_STREET_ADDRESS_2 = '%s' "
-                        + "where PATIENT_UID = %d;", 
+                        + "where PATIENT_UID = %d;",
                         city, state, street_addr1, street_addr2, entity_uid);
-      rdbConnector.query(sql);
+      connector.query(sql);
 
     } catch(JSONException e) {
 		  e.printStackTrace();
 	  } catch(Exception e) {
+      // e.printStackTrace();
+    }
+  }
+
+  public void process_race(String rawjson) {
+    JSONObject json = null;
+    String sql = "";
+    ResultSet result;
+    try {
+      json = new JSONObject(rawjson).getJSONObject("payload");
+      int person_uid = json.getInt("person_uid");
+      //calculate the PATIENT_RACE_CALCULATED
+      //instantiate list of races and categories
+      ArrayList<String> races = new ArrayList<>();
+      sql = "select race_cd from NBS_ODSE.dbo.Person_race where person_uid = " + person_uid;
+      connector.query(sql);
+      result = connector.getResults();
+      String race = "";
+      while (result.next()) {
+        race = result.getString(1);
+        category = result.getString(2);
+        races.add(race);
+      }
+      String decoded_races = new String[races.size()];
+      for (int i = 0; i < races.size(); i++) {
+        sql = "select code_desc_txt from NBS_SRTE.dbo.Race_code where code = " + r;
+        connector.getResults();
+        decoded_races[i] = result.getString(1);
+      }
+      String race_calculated = decoded_races[0];
+      String race_calculated_details = "";
+      for String r in decoded_races:
+        race_calculated_details += r + " | ";
+      if (decoded_races.length > 1) {
+        race_calculated = "Multi-Race";
+      }
+
+      System.out.println(String.format("Calculated Race: %s \n Calculated Race Details: %s", race_calculated, race_calculated_details));
+      String sql = String.format("update RDB.dbo.S_PATIENT "
+                                + "set PATIENT_RACE_CALCULATED = '%s', "
+                                + "PATIENT_RACE_CALC_DETAILS ='%s', "
+                                + from RDB.dbo.S_PATIENT where person_uid = %d" + postal_locator_uid);
+      connector.query(sql);
+      ResultSet result = connector.getResults();
+      int entity_uid = 0;
+      while (result.next()) {
+        entity_uid = result.getInt(1);
+        System.out.println(entity_uid);
+      }
+      sql = String.format("update RDB.dbo.S_PATIENT "
+                        + "set PATIENT_CITY = '%s', "
+                        + "PATIENT_STATE = '%s', "
+                        + "PATIENT_STREET_ADDRESS_1 = '%s', "
+                        + "PATIENT_STREET_ADDRESS_2 = '%s' "
+                        + "where PATIENT_UID = %d;",
+                        city, state, street_addr1, street_addr2, entity_uid);
+      connector.query(sql);
+
+    } catch(JSONException e) {
+      e.printStackTrace();
+    } catch(Exception e) {
       // e.printStackTrace();
     }
   }
